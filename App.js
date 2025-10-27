ce5a824af30e081618d7888198d426aa9b97fb4b
Fernier <fernier.paul@gmail.com>
2025-10-27 14:52:13 +0100

    Login

--- App.js
+++ App.js
@@ -3,13 +3,23 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 import KitScreen from './screens/KitScreen';
 import Home from './screens/Home';
 import Create from './screens/Create';
 import Listen from './screens/Listen';
 import Favoris from './screens/Favoris';
-import Login from './screens/Login'
+import Login from './screens/Login';
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import user from './reducers/users';
+
+const store = configureStore({
+  reducer: { user },
+});
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -28,13 +38,16 @@ const TabNavigator = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from '../reducers/users';
+
 
 export default function Home() {
+
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+
+  console.log(user)
+
   return (
-    <View>
+    <SafeAreaView>
+      <TextInput></TextInput>
       <Text>HOME PAGE</Text>
-    </View>
+    </SafeAreaView>
   );
 }
