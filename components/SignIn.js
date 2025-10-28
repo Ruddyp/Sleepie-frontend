afea4eda5e024041bcf0ca9d7f722e740c7948c7
Fernier <fernier.paul@gmail.com>
2025-10-28 14:59:02 +0100

    loginV2

---
+++ components/SignIn.js
@@ -0,0 +1,113 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, Switch } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from '../reducers/users';
+import {
+    Colors,
+    Typography,
+} from "../components/KitUI/tokens";
+import Button from '../components/KitUI/Button'
+import Input from "../components/KitUI/Input";
+import { useState } from "react";
+import { checkInput } from "../modules/checkInput";
+
+
+export default function SignIn({ navigation }) {
+    const IP = process.env.EXPO_PUBLIC_IP
+    const port = process.env.EXPO_PUBLIC_PORT
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState({ error: false, value: "" })
+    const [password, setPassword] = useState('')
+    const [emptyfield, setEmptyfield] = useState(false)
+    const [messageFromBack, setMessageFromBack] = useState("")
+
+    const handlePress = async () => {
+        setEmptyfield(false);
+
+        const inputFields = [email.value, password];
+        if (!checkInput(inputFields)) return setEmptyfield(true)
+
+        //Check email 
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+        const isValidEmail = (email) => emailRegex.test(email);
+        if (!isValidEmail(email.value)) {
+            setEmail({
+                error: "invalid email",
+                value: email.value
+            })
+            return
+        }
+        const body = {
+            email: email.value,
+            password: password,
+        }
+        setMessageFromBack("")
+        try {
+            const response = await fetch(`http://${IP}:${port}/users/signin`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body)
+            })
+            const data = await response.json()
+            if (data.result) {
+                dispatch(updateUser(data.data))
+                navigation.navigate("TabNavigator")
+            } else {
+                setMessageFromBack(data.error)
+            }
+        } catch (error) {
+            console.log("error from SignIn", error)
+        }
+        setEmail({ error: false, value: "" })
+        setPassword('')
+    }
+    return (
+        <View style={styles.container}>
+            <Input
+                placeholder="Email"
+                onChangeText={(value) => setEmail({
+                    error: false,
+                    value: value
+                })}
+                value={email}
+                error={email.error}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <Input
+                placeholder="Mot de passe"
+                onChangeText={(value) => setPassword(value)}
+                value={password}
+                password
+                autoCapitalize="none"
+            />
+            <View style={styles.button}>
+                <Button
+                    title="Signin"
+                    size="large"
+                    variant="primary"
+                    onPress={() => handlePress()}
+                />
+            </View>
+            {emptyfield && <Text style={styles.errorMessage}>Champ(s) vide(s)</Text>}
+            {messageFromBack && <Text style={styles.errorMessage}>{messageFromBack}(s)</Text>}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    errorMessage: {
+        color: Colors.textTitle,
+        ...Typography.body,
+        fontFamily: Typography.fontBody
+    },
+    container: {
+        padding: 30,
+        justifyContent: "center",
+        gap: 15,
+    },
+    button: {
+        marginTop: 30,
+    },
+})
---
+++ screens/Profil.js
@@ -0,0 +1,90 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, Switch } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+    Colors,
+    Typography,
+} from "../components/KitUI/tokens";
+import Button from '../components/KitUI/Button'
+import { LinearGradient } from "expo-linear-gradient";
+import { useSelector } from "react-redux";
+
+
+export default function Login({ navigation }) {
+
+    const user = useSelector((state) => state.user.value);
+
+    const handlePressDeconnect = () => {
+        //intégrer la route de déconnection + vider le reducer
+        navigation.navigate("Login");
+    }
+    const handlePressDelete = () => {
+        //intégrer la route de suppression de compte + vider le reducer
+        navigation.navigate("Login");
+    }
+
+    return (
+        <SafeAreaView style={styles.area}>
+            <LinearGradient
+                colors={[Colors.bgPrimary[0], Colors.bgPrimary[1]]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.main}>
+                <View style={styles.containerTop}>
+                    <Text style={styles.textTop}>{user.username}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Compte</Text>
+                    <View>
+                        <Text style={styles.textSmall}> Email : {user.email}</Text>
+                    </View>
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Mon abonnement</Text>
+                </View>
+                <View style={styles.containerBottom}>
+                    <Button title="Se déconnecter" onPress={() => handlePressDeconnect()} />
+                    <Button title="Supprimer mon compte" onPress={() => handlePressDelete()} />
+                </View>
+            </LinearGradient>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    area: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+    },
+    main: {
+        flex: 1,
+        alignContent: "center",
+        justifyContent: "center",
+        padding: 15,
+    },
+    container: {
+        flex: 1
+    },
+    text: {
+        ...Typography.h3,
+        color: Colors.textTitle,
+    },
+    containerTop: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textTop: {
+        ...Typography.h2,
+        color: Colors.textTitle,
+    },
+    textSmall: {
+        ...Typography.body,
+        color: Colors.textBody,
+    },
+    containerBottom: {
+        padding: 10,
+        flex: 1,
+        gap: 10,
+    }
+})
